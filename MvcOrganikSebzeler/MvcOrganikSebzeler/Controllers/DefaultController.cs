d4a033ee4013504c52135556f5bd8c0fd63dfd73
Furkan <furkannkayaa90@gmail.com>
2024-08-13 08:55:27 +0300

    Mvc Organik Meyveler

---
+++ MvcOrganikSebzeler/MvcOrganikSebzeler/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOrganikSebzeler.Models.Entity;
+namespace MvcOrganikSebzeler.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MvcOrganikEntities db=new MvcOrganikEntities();
+        public ActionResult Index()
+        {
+            var degerler=db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+    }
+}
